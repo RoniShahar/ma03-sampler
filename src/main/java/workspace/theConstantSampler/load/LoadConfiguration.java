69b3b716f31ca14b09806ae71f3855fff8cbeebc
roni shahar <ronishahar21@gmail.com>
2022-01-16 17:39:17 +0200

    create configuration file and LoadConfiguration class

---
+++ src/main/java/workspace/theConstantSampler/load/LoadConfiguration.java
@@ -0,0 +1,26 @@
+package workspace.theConstantSampler.load;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class LoadConfiguration extends Load{
+
+    public LoadConfiguration() {
+        super("src/main/resources/properties.properties");
+    }
+
+    public Properties loadConfiguration() throws IOException {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(super.path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Properties prop = new Properties();
+        prop.load(fileInputStream);
+        return prop;
+    }
+}
